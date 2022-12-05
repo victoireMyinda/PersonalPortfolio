cf1d0602a0fc567228f578e438704ad3d6e2a0cb
vict97 <myindavictoire@gmail.com>
2022-12-05 15:43:04 +0100

    ajout de dossier portfolio

---
+++ src/Portfolio/Home/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='profile-contaire'>
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="colz">
+                        <a href='#'>
+                            <i className="fa-brands fa-github"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa-brands fa-twitter"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Profile;
